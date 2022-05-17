86626737678c74a21eab8bd730b9c78af493715e
Mladen Draganović <mladenoneacc@gmail.com>
2022-05-17 13:57:30 +0200

    🔌 Added required Angular Material and CDK

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,45 @@ import { BrowserModule } from '@angular/platform-browser'
 
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
+import { HeaderComponent } from './components/header/header.component'
+import { FooterComponent } from './components/footer/footer.component'
+
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatListModule } from '@angular/material/list'
+import { MatGridListModule } from '@angular/material/grid-list'
+import { MatExpansionModule } from '@angular/material/expansion'
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { AboutComponent } from './pages/about/about.component'
+import { HomeComponent } from './pages/home/home.component'
+import { NationalitiesComponent } from './pages/nationalities/nationalities.component'
+import { TeamsComponent } from './pages/teams/teams.component'
+import { LeaguesComponent } from './pages/leagues/leagues.component'
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    NationalitiesComponent,
+    TeamsComponent,
+    LeaguesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatGridListModule,
+    MatExpansionModule,
+    BrowserAnimationsModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
