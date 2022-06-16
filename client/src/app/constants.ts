337110571ed3634e4fe2c27e467ccf177688cb89
Mladen Draganović <mladenoneacc@gmail.com>
2022-06-16 19:30:09 +0200

    🔧 Updated frontend config

--- client/src/app/constants.ts
+++ client/src/app/constants.ts
@@ -1,5 +1,6 @@
-export const NATIONALITIES_URI = 'http://localhost:8083/nacionalnost'
-export const LEAGUES_URI = 'http://localhost:8083/liga'
-export const TEAMS_URI = 'http://localhost:8083/tim'
-export const PLAYERS_URI = 'http://localhost:8083/igrac'
-export const PLAYER_TEAM_URI = 'http://localhost:8083/igrac/tim'
+export const NATIONALITIES_URI =
+  'https://rva-it68g2019.herokuapp.com/nacionalnost'
+export const LEAGUES_URI = 'https://rva-it68g2019.herokuapp.com/liga'
+export const TEAMS_URI = 'https://rva-it68g2019.herokuapp.com/tim'
+export const PLAYERS_URI = 'https://rva-it68g2019.herokuapp.com/igrac'
+export const PLAYER_TEAM_URI = 'https://rva-it68g2019.herokuapp.com/igrac/tim'
